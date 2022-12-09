56a8054369c0ca23d9d9ac79d64144b3de8f0998
rohityellapu <rohithyellapu24@gmail.com>
2022-12-09 19:30:44 +0530

    Added Navigator Succesfully but adding tailwind css(i.e. nativewind) requires some more configs

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+import { NavigationContainer } from '@react-navigation/native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={ { flex: 1, alignItems: 'center', justifyContent: 'center' } }>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ HomeScreen } />
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+
   );
 }
 
