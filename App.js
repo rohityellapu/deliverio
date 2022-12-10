344ce30f06e96094c5307a767f7f9485d7e25068
rohityellapu <rohithyellapu24@gmail.com>
2022-12-10 06:39:39 +0530

    solved tailwind issue

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
 import { NativeWindStyleSheet } from "nativewind";
 import "./main.css"
 NativeWindStyleSheet.setOutput({
@@ -7,31 +7,23 @@ NativeWindStyleSheet.setOutput({
 });
 
 import { NavigationContainer } from '@react-navigation/native';
-import { TailwindProvider } from 'tailwindcss-react-native';
+
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={ HomeScreen } />
-        </Stack.Navigator>
-      </TailwindProvider>
+
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ HomeScreen } />
+      </Stack.Navigator>
+
     </NavigationContainer>
 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
