81eeddeb422c5cc735dbba5d9508003931262ec1
rohityellapu <rohithyellapu24@gmail.com>
2022-12-30 11:32:40 +0530

    added required screens for now

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
+import { Platform } from 'react-native';
 
-import HomeScreen from './screens/HomeScreen';
-import { NativeWindStyleSheet } from "nativewind";
-
-// For webpack
-import "./main.css"
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
+// For webpack (comment it if you are using Android or iOS SDK) 
+import { NativeWindStyleSheet } from 'nativewind';
+if (Platform.OS != 'android') {
+  require('./main.css')
+  NativeWindStyleSheet.setOutput({
+    default: "native",
+  });
+}
 
 import { NavigationContainer } from '@react-navigation/native';
-
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import RestaurantScreen from './screens/RestaurantScreen';
+import HomeScreen from './screens/HomeScreen';
+import { Provider } from 'react-redux';
+import store from './store';
+import BasketScreen from './screens/BasketScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
 
     <NavigationContainer>
-
+      <Provider store={store}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={ HomeScreen } />
+        <Stack.Screen name="Restaurant" component={ RestaurantScreen } />
+          <Stack.Screen name="Basket" component={ BasketScreen }
+            options={{presentation:'modal', headerShown:false}}
+          />
       </Stack.Navigator>
-
+      </Provider>
     </NavigationContainer>
 
   );
